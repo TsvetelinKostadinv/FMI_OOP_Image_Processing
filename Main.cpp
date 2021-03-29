16e79715b9b21cae185d17185af6308900c39f08
Tsvetelin Kostadinov <tsvetelinkostadinovts@gmail.com>
2021-03-29 16:32:15 +0300

    Modified the build file to reference the libraries

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,20 @@
 ﻿#include <iostream>
+#include "rlutil.h"
+
+#define waitkey rlutil::anykey("\nPress any key to continue...\n")
+
+#define RESET_COLOR_AFTER_SECTION(x){\
+rlutil::saveDefaultColor();\
+##x; \
+rlutil::resetColor();\
+};
 
 int main()
 {
-	std::cout << "Welcome to the Image processor!" << std::endl;
-	std::cin.get();
+	RESET_COLOR_AFTER_SECTION({
+		rlutil::setColor(14);
+		std::cout << "Welcome to the Image processor!" << std::endl;
+	})
+	waitkey;
 	return 0;
 }
---
+++ README.md
@@ -0,0 +1,10 @@
+# Обработка на изображения
+Проект по курса във ФМИ ООП 2021
+
+## За мен
+Име: Цветелин Цецков
+ФН: 82130
+Крус, група: КН, 1 курс, 1 група
+
+## Повече за проекта
+https://docs.google.com/document/d/11ljQpn64rduAtHenQVp2Sx75Au2_gpzDerfDl3v9_kc/edit#heading=h.dofhyzxkwfex
